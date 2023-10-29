f8edbd22fc5572a599ea16e3d9e9f46c4627f19a
awanklod <137737752+awanklod@users.noreply.github.com>
2023-10-30 03:04:39 +0700

    Add files via upload

---
+++ instal-udp.sh
@@ -0,0 +1,5 @@
+echo -e " [INFO] Sabar sayang Lagi Proses"
+wget https://raw.githubusercontent.com/awanklod/alvi/main/tunnel/udp.sh && bash udp.sh
+echo -e " [INFO] UDP Sukses Di Pasang"
+sleep 2
+menu
